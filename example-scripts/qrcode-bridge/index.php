3046c112e6bb74ab5012b8a7ea38bfa297499dd1
luri <luri@kioss.com>
2022-02-01 11:19:20 +0700

    QRCode Bridge

---
+++ example-scripts/qrcode-bridge/index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * USAGE
+ *   curl -X POST "localhost:8000/" -d '{"botName":"YourBot","phoneNumber":"62112233445566","clientId":1,"code":"1@airMsIMe6ASkRoWnG+2gY1t43MOgmi0nEkAJGf+uIFyud5LMHKXD"}'
+ */
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+@header("Content-type:application/json");
+$json['code'] = 400;
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+	$json['text'] = 'Invalid method.';
+	die(json_encode( $json));
+}
+
+$RequestContent = file_get_contents('php://input');
+$RequestContentAsJson = json_decode($RequestContent, true);
+$BotName = $RequestContentAsJson['botName'];
+$PhoneNumber = $RequestContentAsJson['phoneNumber'];
+$ClientId = $RequestContentAsJson['clientId'];
+$QRCode = $RequestContentAsJson['code'];
+
+if ((empty($QRCode)) or (empty($BotName))){
+	$json['text'] = 'Invalid parameters.';
+	die(json_encode( $json));
+}
+
+// Your code here ..
+//
+//
+
+
+
+// Response
+$json['code'] = 0;
+$json['text'] = 'OK';
+$outputJson = json_encode( $json);
+echo $outputJson;
+
